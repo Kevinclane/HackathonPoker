67d9d398cd2b015152a17cd668579a0644392f70
Kevin Lane <59802468+Kevinclane@users.noreply.github.com>
2021-08-04 22:16:19 -0600

    added to landing page

--- clientsrc/src/views/Home.vue
+++ clientsrc/src/views/Home.vue
@@ -20,6 +20,33 @@
           <button v-else class="btn btn-info" @click="login">Login</button>
         </div>
       </div>
+      <div class="about-row row super-center my-3">
+        <div class="col-10 offset-1">
+          <h4 class="my-3">
+            Thanks for taking some time to check out my project! My name is
+            Kevin Lane and I'm a fullstack developer. I was tasked with building
+            a card game for a hackathon and this is what I came up with. I built
+            it with a Vue front-end and Node.js + Express back-end with MongoDB
+            for the database.
+          </h4>
+          <h4 class="my-3">
+            This is the first time I've really used Socket.io. It was essential
+            in order to create a multi-player game. I also had to create an
+            event loop to handle the game's functionality (also a first for me).
+            That was the most challenging and rewarding part of this entire
+            build. I made a diagram of this process so I could better keep track
+            of what's going on and be able to find/fix bugs + decide where
+            upgrades can be made. I saved a copy of this diagram in the root
+            file of this folder. A link to the repo is in the top right corner
+            of the screen.
+          </h4>
+          <h4>
+            I'm currently doing freelance work, but am actively seeking a
+            full-time position. Feel free to reach out to me via LinkedIn (link
+            also in the top right corner).
+          </h4>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -85,7 +112,14 @@ export default {
 .game-row {
   width: 80vw;
   height: 10vh;
-  background-color: rgb(176 196 222 / 0.7);
+  background-color: rgb(176 196 222 / 0.9);
+  border-radius: 20px;
+}
+
+.about-row {
+  width: 80vw;
+  height: 50vh;
+  background-color: rgb(176 196 222 / 0.9);
   border-radius: 20px;
 }
 </style>
